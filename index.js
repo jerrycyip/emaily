0d785d888be226038f316820f02aa36a1f016b67
Jerry Yip <jerry.yip@gmail.com>
2021-04-25 18:30:40 -0700

    finish auth flow

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require("./models/User");
+require("./services/passport");
+
 const app = express();
 
-app.get('/', (req, res) => {
- res.send({ bye: 'buddy' });
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 *1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 const PORT = process.env.PORT || 5000;
